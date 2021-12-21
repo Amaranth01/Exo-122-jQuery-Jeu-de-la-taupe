62be014e3c736afb92ee316136af06ccb6f302e9
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-21 15:14:46 +0100

    o

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole'); //trous
+const scoreBoard = $('.score');
+const moles = $('.mole'); //taupe
+let lastHole = 0;
+
+//choose random holes
+function randomHole() {
+    const r = Math.floor(Math.random() * 6);
+    return $(holes.get(r));
+}
+
+//up the hole
+function hello () {
+    const hole = randomHole();
+    hole.addClass('up');
+    holes.fadeTo(800);
+    holes.click(function (){
+        $(this).removeClass('up');
+        scoreBoard.text(parseInt(scoreBoard.text())+1);
+    });
+}
+
+//calcul the time
+
+setTimeout(function(){
+    let time = 0;
+    time++;
+    if (time === 10) {
+        clearTimeout()
+        alert('fin de partie');
+    }
+});
+
+$('button').click(function startGame(){
+    if( $('.hole.up').get().length === 0) {
+        lastHole = 0;
+        scoreBoard.text(0);
+        hello();
+    }
+});
